96ef426aed6757625efacaf3a98abc93c4275ab8
Mosuzzzz <180250518+Mosuzzzz@users.noreply.github.com>
2025-11-29 10:06:27 +0700

    Add AdSense configuration and ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <head />
+      <body className={cn(inter.className, "min-h-screen bg-slate-50 font-sans antialiased leading-relaxed text-slate-900")}>
         {/* Google AdSense */}
         <Script
           async
@@ -29,8 +30,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-      </head>
-      <body className={cn(inter.className, "min-h-screen bg-slate-50 font-sans antialiased leading-relaxed text-slate-900")}>
         <div className="relative flex min-h-screen flex-col">
           <Navbar />
           <main className="flex-1 container py-6">{children}</main>
